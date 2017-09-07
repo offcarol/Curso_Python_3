7b1a8eeb831a892101886cc93e4130129412e124
Carolina <offcarolina@hotmail.com>
2017-09-07 01:22:22 +0000

    Nomes dos arquivos alterados para melhor organização

--- _Aula_01/desafio_002.py
+++ _Aula_01/desafio_002.py
@@ -1,8 +1,8 @@
 #Crie um script Python que leia dois números e tente
 #mostrar a soma entre eles.
-#exercício em andamento
-n1 = input('Digite o primeiro número: ')
-n2 = input('Digite o segundo número: ')
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
 s = n1 + n2
 
 print('A soma de ', n1 , '+' , n2 , 'é igual a: ' , s , '.')
